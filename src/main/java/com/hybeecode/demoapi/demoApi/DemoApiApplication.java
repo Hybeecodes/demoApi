25f27ec4a3a4fb51083fa56b4d35dbd33b5c1d45
Niyi <obikoya11@gmail.com>
2019-09-11 10:42:10 +0100

    first commit

---
+++ src/main/java/com/hybeecode/demoapi/demoApi/DemoApiApplication.java
@@ -0,0 +1,14 @@
+package com.hybeecode.demoapi.demoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiApplication.class, args);
+	}
+
+
+}
